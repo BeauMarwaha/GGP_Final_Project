18b00dda9875b3ea040efab80d235cd404c2d5f2
KatWeis <kxw4589@g.rit.edu>
2018-10-30 18:24:36 -0400

    Started on asteroids

---
+++ DX11Starter/Asteroid.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Entity.h"
+
+// --------------------------------------------------------
+// An Asteroid class that represents a singular asteroid object
+// Inherits from the Entity class
+// --------------------------------------------------------
+class Asteroid :
+	public Entity
+{
+public:
+	Asteroid();
+	~Asteroid();
+};
+
