0adc284bdb1272cc46016e4005e59b6b17c720e5
KatWeis <kxw4589@g.rit.edu>
2018-12-12 15:18:12 -0500

    Added in needed shader files (still need to be coded)

---
+++ DX11Starter/PixelShaderBloom.hlsl
@@ -0,0 +1,12 @@
+// Defines the input to this pixel shader
+// - Should match the output of our corresponding vertex shader
+struct VertexToPixel
+{
+	float4 position		: SV_POSITION;
+	float2 uv           : TEXCOORD0;
+};
+
+float4 main(VertexToPixel input) : SV_TARGET
+{
+	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+}
