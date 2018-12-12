fd6a5df6ae014ca1b35384ae8b8c39d29ee56ba9
KatWeis <kxw4589@g.rit.edu>
2018-12-12 15:49:50 -0500

    small edits to extraction

--- DX11Starter/PixelShaderExtractBright.hlsl
+++ DX11Starter/PixelShaderExtractBright.hlsl
@@ -6,7 +6,13 @@ struct VertexToPixel
 	float2 uv           : TEXCOORD0;
 };
 
+// Textures and such
+Texture2D Pixels		: register(t0);
+SamplerState Sampler	: register(s0);
+
 float4 main(VertexToPixel input) : SV_TARGET
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	// Retrieve the current color of the pixel after normal rendering
+	float4 og = Pixels.Sample(Sampler, input.uv);
+	return float4(1.0f, 1.0f, 1.0f, 1.0f);	
 }
